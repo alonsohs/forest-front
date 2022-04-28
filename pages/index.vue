0fdb8f77849f01c31a669a9409ed7eb4e8f5b5a2
Alonso Hernández Sastré <alonsohdez14@gmail.com>
2022-04-28 18:22:34 -0500

    Add: app layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,21 @@
 <template>
-  <Tutorial />
+  <div class="w-full bg-white rounded-md shadow-md text-center">
+    Hola Mundo
+
+    {{$auth.user}}
+    <button @click="userLogout">Logout</button>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'IndexPage',
+  layout: 'app',
+  methods: {
+    userLogout() {
+      this.$auth.logout()
+    },
+
+  }
 }
 </script>
