d59c5c17cb35dd6db921e4c57e0f795b3fae5f74
Fernando <osocaramelosofer@hotmail.com>
2022-04-27 19:39:28 -0500

    WIP add login styles

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -1,11 +1,19 @@
 <template>
-  <div class="">
-    <span>Login</span>
-    <form @submit.prevent="userLogin" class="flex flex-col">
-      <input v-model="email" placeholder="Email">
-      <input v-model="password" placeholder="Password">
-      <button type="submit">Login</button>
-    </form>
+  <div class="grid grid-cols-2 h-screen">
+    <div class="bg-hero ">
+      <h1>Login</h1>
+    </div>
+    <div>
+      <div class="object-cover w-28 h-28">
+        <img class="h-full w-full" src="@/assets/blue-logo.svg" >
+      </div>
+      <span>Login</span>
+      <form @submit.prevent="userLogin" class="flex flex-col">
+        <input v-model="email" placeholder="Email">
+        <input v-model="password" placeholder="Password">
+        <button type="submit">Login</button>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -35,5 +43,7 @@ export default {
 </script>
 
 <style scoped>
-
+.bg-hero{
+  background-image: linear-gradient(to top, #160d38, #1a124c, #1c1661, #1c1b76, #19208d, #1736a5, #104cbd, #0062d5, #0088e9, #00a8ed, #00c6ea, #5ce1e6);
+}
 </style>
