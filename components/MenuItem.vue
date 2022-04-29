0dda07f2292190c2f6f3fdbdc9644a7eb4164393
Fernando <osocaramelosofer@hotmail.com>
2022-04-29 14:32:26 -0500

    Create components for the menu

---
+++ components/MenuItem.vue
@@ -0,0 +1,29 @@
+<template>
+  <a :href="to" class="text-gray-300 hover:bg-gray-700 hover:text-white group flex items-center px-2 py-2 text-base font-medium rounded-md">
+    <div class="object-cover w-8 h-8">
+      <img class="h-full w-full" :src="require(`@/assets/${image}`)" :alt="alt" />
+    </div>
+    <span class="ml-2">{{ name }}</span>
+  </a>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {type:String, default:''},
+    alt: {type:String, default:''},
+    image: {type:String, default:''},
+    to: {type:String, default:''},
+
+  },
+ data(){
+   return{
+
+   }
+ }
+}
+</script>
+
+<style scoped>
+
+</style>
